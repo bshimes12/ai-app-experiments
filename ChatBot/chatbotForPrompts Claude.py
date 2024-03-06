ead09823519db207c4635472841af530c30528a5
bshimes12 <33007658+bshimes12@users.noreply.github.com>
2024-03-06 16:09:32 -0500

    remove key

--- ChatBot/chatbotForPrompts Claude.py
+++ ChatBot/chatbotForPrompts Claude.py
@@ -9,7 +9,7 @@ import promptTheBestCoach as promptTheBestCoach
 #set_environment()
 
 
-my_api_key = "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"
+my_api_key = ""
 client = anthropic.Anthropic(
     # defaults to os.environ.get("ANTHROPIC_API_KEY")
     api_key=my_api_key
